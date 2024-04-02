27bab2ce98ec7ebb36a54bbef0ead53055d285c8
Picalines <picalines0@gmail.com>
2024-04-02 20:22:11 +0300

    pages: sign-in on index

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
+
 export default function Home() {
-	return <main></main>
+	return (
+		<main className="w-full">
+			<Link
+				href="/sign-in"
+				className={buttonVariants({ variant: 'default' })}
+			>
+				Sign In
+			</Link>
+		</main>
+	)
 }
