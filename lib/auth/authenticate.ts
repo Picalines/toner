1e5ed5b7fcda89fc1d2e50891dd7ab00754bfd1f
Picalines <picalines0@gmail.com>
2024-04-02 23:20:29 +0300

    auth: cache(authenticate)

--- lib/auth/authenticate.ts
+++ lib/auth/authenticate.ts
@@ -1,11 +1,12 @@
-import { lucia } from '.'
 import { Cookie, Session, User } from 'lucia'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { cache } from 'react'
+import { lucia } from '.'
 
 export type UserAuth = { user: User; session: Session }
 
-export const authenticate = async (): Promise<UserAuth | null> => {
+export const authenticate = cache(async (): Promise<UserAuth | null> => {
 	const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null
 	if (!sessionId) {
 		return null
@@ -33,7 +34,7 @@ export const authenticate = async (): Promise<UserAuth | null> => {
 	} catch {}
 
 	return user && session ? { user, session } : null
-}
+})
 
 export const authenticateOrRedirect = async (
 	redirectPath: string,
