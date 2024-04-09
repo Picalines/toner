86eb79ac59f5951818caa49e94a06b0de11a8747
Picalines <picalines0@gmail.com>
2024-04-09 21:30:11 +0300

    components: iconOnly ThemeToggle variant

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -3,11 +3,19 @@
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
-import { capitalize } from '@/lib/utils'
+import { capitalize, cn } from '@/lib/utils'
 import { Button, ButtonProps } from './ui/button'
 import { Skeleton } from './ui/skeleton'
 
-export default function ThemeToggle(props: ButtonProps) {
+type Props = Omit<ButtonProps, 'onClick'> &
+	Readonly<{
+		toggleVariant?: 'all' | 'iconOnly'
+	}>
+
+export default function ThemeToggle({
+	toggleVariant = 'all',
+	...buttonProps
+}: Props) {
 	const { setTheme, theme } = useTheme()
 	const [isMounted, setIsMounted] = useState(false)
 
@@ -18,7 +26,11 @@ export default function ThemeToggle(props: ButtonProps) {
 	}
 
 	return (
-		<Button {...props} onClick={toggleTheme}>
+		<Button
+			{...buttonProps}
+			className={cn('space-x-2', buttonProps.className)}
+			onClick={toggleTheme}
+		>
 			{isMounted ? (
 				<span className="relative w-[24px] h-[24px]">
 					<Sun className="absolute transition-all scale-100 rotate-0 dark:scale-0 dark:rotate-90" />
@@ -27,11 +39,13 @@ export default function ThemeToggle(props: ButtonProps) {
 			) : (
 				<Skeleton className="w-[24px] h-[24px] rounded-full shrink-0" />
 			)}
-			{isMounted ? (
-				<span>{capitalize(theme ?? '')}</span>
-			) : (
-				<Skeleton className="h-4 w-full" />
-			)}
+			{toggleVariant == 'all' ? (
+				isMounted ? (
+					<span>{capitalize(theme ?? '')}</span>
+				) : (
+					<Skeleton className="h-4 w-full" />
+				)
+			) : null}
 		</Button>
 	)
 }
