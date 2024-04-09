743e8fbc61be450fcc6990b94bd45dcf032e1137
Picalines <picalines0@gmail.com>
2024-04-09 18:02:27 +0300

    ui: ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { PropsWithChildren } from 'react'
+import { ThemeProvider } from '@/components/theme-provider'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +12,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
+		<html lang="en" suppressHydrationWarning>
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	)
 }
