76199d2591096c66488f0a43d07b8f197ff6ac32
Picalines <picalines0@gmail.com>
2024-04-09 19:47:03 +0300

    move sign-out back to /account

--- app/(user)/account/page.tsx
+++ app/(user)/account/page.tsx
@@ -1,4 +1,7 @@
+import { LogOut } from 'lucide-react'
 import { authenticateOrRedirect } from '@/lib/auth'
+import { Button } from '@/components/ui/button'
+import { signOut } from '@/actions/auth/sign-out'
 
 export default async function AccountPage() {
 	const {
@@ -8,6 +11,12 @@ export default async function AccountPage() {
 	return (
 		<div>
 			logged in as {login} (#{userId})!
+			<form action={signOut}>
+				<Button type="submit">
+					<LogOut />
+					<span>Sign Out</span>
+				</Button>
+			</form>
 		</div>
 	)
 }
