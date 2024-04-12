b201a06a996ac2d02ba55a9b47e7263d1fb088c5
Picalines <picalines0@gmail.com>
2024-04-12 23:18:27 +0300

    pages: basic /account

--- app/(user)/account/page.tsx
+++ app/(user)/account/page.tsx
@@ -1,22 +1,46 @@
-import { LogOut } from 'lucide-react'
+import { LogOut, Pencil } from 'lucide-react'
+import { ComponentProps } from 'react'
 import { authenticateOrRedirect } from '@/lib/auth'
 import { Button } from '@/components/ui/button'
+import UserAvatar from '@/components/user-avatar'
 import { signOut } from '@/actions/auth/sign-out'
 
 export default async function AccountPage() {
 	const {
-		user: { id: userId, login },
+		user: { login, displayName },
 	} = await authenticateOrRedirect('/sign-in')
 
 	return (
-		<div>
-			logged in as {login} (#{userId})!
-			<form action={signOut}>
-				<Button type="submit">
-					<LogOut />
-					<span>Sign Out</span>
+		<div className="p-2">
+			<div className="w-min space-y-2">
+				<div className="flex flex-row items-center gap-2 w-min">
+					<UserAvatar
+						login={login}
+						displayName={displayName}
+						className="w-16 h-16 text-3xl"
+					/>
+					<div className="flex flex-col gap-1">
+						<span>{login}</span>
+						<span className="text-gray-500">@{displayName}</span>
+					</div>
+				</div>
+				<Button variant="outline" className="space-x-2 w-full">
+					<Pencil width={16} />
+					<span>Edit Profile</span>
 				</Button>
-			</form>
+			</div>
+			<SignOut className="absolute right-4 bottom-4" />
 		</div>
+	)
+}
+
+function SignOut(formProps: ComponentProps<'form'>) {
+	return (
+		<form {...formProps} action={signOut}>
+			<Button type="submit" variant="outline" className="space-x-2">
+				<LogOut />
+				<span>Sign Out</span>
+			</Button>
+		</form>
 	)
 }
---
+++ components/user-avatar.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { AvatarImage } from '@radix-ui/react-avatar'
+import { Avatar, AvatarFallback } from './ui/avatar'
+
+type Props = Readonly<{
+	login: string
+	displayName: string
+	imageUrl?: string
+	className?: string
+	fallbackClassName?: string
+}>
+
+export default function UserAvatar({
+	login,
+	displayName,
+	imageUrl,
+	className,
+	fallbackClassName,
+}: Props) {
+	return (
+		<Avatar className={className}>
+			<AvatarImage src={imageUrl} alt={login} />
+			<AvatarFallback className={fallbackClassName}>
+				{displayName
+					.split(' ', 2)
+					.map(p => p[0].toUpperCase())
+					.join()}
+			</AvatarFallback>
+		</Avatar>
+	)
+}
