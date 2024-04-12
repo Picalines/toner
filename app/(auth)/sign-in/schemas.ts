34b96361f09629ceb91ff5b616738e4d0231045f
Picalines <picalines0@gmail.com>
2024-04-12 21:27:30 +0300

    fix: shorten error messages

--- app/(auth)/sign-in/schemas.ts
+++ app/(auth)/sign-in/schemas.ts
@@ -3,16 +3,16 @@ import { z } from 'zod'
 export const signInFormSchema = z.object({
 	login: z
 		.string()
-		.min(5, 'login must contain at least 5 characters')
-		.max(32, 'login must contain at most 32 characters')
+		.min(5, 'must contain at least 5 characters')
+		.max(32, 'must contain at most 32 characters')
 		.regex(
 			/^[a-z0-9_]+$/,
-			'login must consist of only lowercase a-z characters, digits or an underscore',
+			'must consist of only lowercase a-z characters, digits or an underscore',
 		),
 	password: z
 		.string()
-		.min(6, 'password must contain at least 6 characters')
-		.max(100, 'password must contain at most 255 characters'),
+		.min(6, 'must contain at least 6 characters')
+		.max(100, 'must contain at most 255 characters'),
 })
 
 export type SignInFormData = z.infer<typeof signInFormSchema>
