b18f9a05a30f3909e46bf648f667cae1e73b012c
Picalines <picalines0@gmail.com>
2024-04-17 14:17:21 +0300

    ui: /account actions menu

--- app/(user)/account/page.tsx
+++ app/(user)/account/page.tsx
@@ -1,32 +1,76 @@
-import { Pencil } from 'lucide-react'
+import { User } from 'lucia'
+import { EllipsisIcon, KeyRoundIcon, UserIcon } from 'lucide-react'
+import Link from 'next/link'
 import { authenticateOrRedirect } from '@/lib/auth'
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover'
+import { Separator } from '@/components/ui/separator'
 import UserAvatar from '@/components/user-avatar'
 
 export default async function AccountPage() {
-	const {
-		user: { login, displayName },
-	} = await authenticateOrRedirect('/sign-in')
+	const { user } = await authenticateOrRedirect('/sign-in')
 
 	return (
 		<div className="p-2">
-			<div className="w-min space-y-2">
-				<div className="flex w-min flex-row items-center gap-2">
-					<UserAvatar
-						login={login}
-						displayName={displayName}
-						className="h-16 w-16 text-3xl"
-					/>
-					<div className="flex flex-col gap-1">
-						<span>{login}</span>
-						<span className="text-gray-500">@{displayName}</span>
-					</div>
-				</div>
-				<Button variant="outline" className="w-full space-x-2">
-					<Pencil width={16} />
-					<span>Edit Profile</span>
-				</Button>
+			<PageHeader {...user} />
+			<Separator className="mb-2 mt-2" />
+		</div>
+	)
+}
+
+function PageHeader({ login, displayName }: User) {
+	return (
+		<div className="flex flex-row items-center p-2">
+			<div className="flex flex-row items-center gap-2">
+				<UserAvatar
+					login={login}
+					displayName={displayName}
+					className="h-16 w-16 text-3xl"
+				/>
+				<span>
+					{login == displayName ? (
+						<>@{login}</>
+					) : (
+						<>
+							{displayName} @{login}
+						</>
+					)}
+				</span>
 			</div>
+			<div className="flex-grow" />
+			<Popover>
+				<PopoverTrigger asChild>
+					<Button variant="outline" className="p-2">
+						<EllipsisIcon />
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent className="w-min p-2">
+					<Link
+						href="/account/profile"
+						className={buttonVariants({
+							variant: 'ghost',
+							className: 'w-full space-x-2',
+						})}
+					>
+						<UserIcon />
+						<span>Edit Profile</span>
+					</Link>
+					<Link
+						href="/account/password"
+						className={buttonVariants({
+							variant: 'ghost',
+							className: 'w-full space-x-2',
+						})}
+					>
+						<KeyRoundIcon />
+						<span>Change Password</span>
+					</Link>
+				</PopoverContent>
+			</Popover>
 		</div>
 	)
 }
