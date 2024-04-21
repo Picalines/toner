d3f41211e14d05315b4400620efe11b26da2f2ad
Picalines <picalines0@gmail.com>
2024-04-21 15:47:09 +0300

    util: add tw`` for tailwind LSP

--- lib/utils/styling.ts
+++ lib/utils/styling.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
+
+export function tw(
+	strings: readonly string[] | ArrayLike<string>,
+	...values: readonly any[]
+) {
+	return String.raw({ raw: strings }, ...values)
+}
