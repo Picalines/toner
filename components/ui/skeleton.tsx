d6d6d8e9def2bb8e9d1798aba2cb63e95a7fbb82
Picalines <picalines0@gmail.com>
2024-04-24 00:12:58 +0300

    fix: use ComponentProps instead of HTMLDivElement

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,13 +1,11 @@
+import type { ComponentProps } from 'react'
 import { cn } from '@/lib/utils'
 
-function Skeleton({
-	className,
-	...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+function Skeleton({ className, ...divProps }: ComponentProps<'div'>) {
 	return (
 		<div
 			className={cn('animate-pulse rounded-md bg-muted', className)}
-			{...props}
+			{...divProps}
 		/>
 	)
 }
