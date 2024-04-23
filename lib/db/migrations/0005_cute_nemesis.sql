c9de7c7ebb7861914d725bd910d17eba7cd6c37d
Picalines <picalines0@gmail.com>
2024-04-23 22:12:09 +0300

    fix: 0005 & 0006 migrations

--- lib/db/migrations/0005_cute_nemesis.sql
+++ lib/db/migrations/0005_cute_nemesis.sql
@@ -1,7 +1,7 @@
-DROP TABLE "node_socket";--> statement-breakpoint
-ALTER TABLE "node_connection" DROP CONSTRAINT "node_connection_sender_id_node_socket_id_fk";
+DROP TABLE "node_socket" CASCADE;--> statement-breakpoint
+ALTER TABLE "node_connection" DROP CONSTRAINT IF EXISTS "node_connection_sender_id_node_socket_id_fk";
 --> statement-breakpoint
-ALTER TABLE "node_connection" DROP CONSTRAINT "node_connection_receiver_id_node_socket_id_fk";
+ALTER TABLE "node_connection" DROP CONSTRAINT IF EXISTS "node_connection_receiver_id_node_socket_id_fk";
 --> statement-breakpoint
 ALTER TABLE "node_connection" ADD COLUMN "output_socket" integer NOT NULL;--> statement-breakpoint
 ALTER TABLE "node_connection" ADD COLUMN "input_socket" integer NOT NULL;--> statement-breakpoint
