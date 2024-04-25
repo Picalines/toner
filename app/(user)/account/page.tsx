8280f13873142bbde179151e1b1ac2a092e151ad
Picalines <picalines0@gmail.com>
2024-04-25 13:25:14 +0300

    refactor: extract ProfileSection in /account

--- app/(user)/account/page.tsx
+++ app/(user)/account/page.tsx
@@ -1,99 +1,14 @@
-import { User } from 'lucia'
-import {
-	EllipsisIcon,
-	KeyRoundIcon,
-	LogOutIcon,
-	PencilIcon,
-} from 'lucide-react'
-import Link from 'next/link'
-import { PropsWithChildren } from 'react'
 import { authenticateOrRedirect } from '@/lib/auth'
-import { tw } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import { Separator } from '@/components/ui/separator'
-import UserAvatar from '@/components/user-avatar'
+import ProfileSection from './profile-section'
 
 export default async function AccountPage() {
 	const { user } = await authenticateOrRedirect('/sign-in')
 
 	return (
 		<div className="p-2">
-			<UserProfile {...user} />
+			<ProfileSection {...user} />
 			<Separator className="mb-2 mt-2" />
 		</div>
-	)
-}
-
-function UserProfile({ login, displayName }: User) {
-	return (
-		<div className="flex flex-row items-center p-2">
-			<div className="flex flex-row items-center gap-2">
-				<UserAvatar
-					login={login}
-					displayName={displayName}
-					className="h-16 text-2xl"
-				/>
-
-				{login == displayName ? (
-					<span>@{login}</span>
-				) : (
-					<div className="flex flex-col">
-						<span>{displayName}</span>
-						<span>@{login}</span>
-					</div>
-				)}
-			</div>
-			<div className="flex-grow" />
-			<ProfileDropdownMenu>
-				<Button variant="outline" className="p-2">
-					<EllipsisIcon />
-				</Button>
-			</ProfileDropdownMenu>
-		</div>
-	)
-}
-
-function ProfileDropdownMenu({ children }: PropsWithChildren) {
-	const linkClassName = tw`flex h-min w-full justify-start gap-2 p-2 hover:cursor-pointer`
-
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuLabel>Profile</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem asChild>
-					<Link href="/profile" className={linkClassName}>
-						<PencilIcon />
-						<span>Edit Profile</span>
-					</Link>
-				</DropdownMenuItem>
-				<DropdownMenuItem asChild>
-					<Link href="/account/password" className={linkClassName}>
-						<KeyRoundIcon />
-						<span>Change Password</span>
-					</Link>
-				</DropdownMenuItem>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem asChild>
-					<Link
-						href="/sign-out"
-						scroll={false}
-						className={linkClassName}
-					>
-						<LogOutIcon />
-						<span>Sign out</span>
-					</Link>
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
 	)
 }
---
+++ app/(user)/account/profile-section.tsx
@@ -0,0 +1,95 @@
+import {
+	EllipsisIcon,
+	KeyRoundIcon,
+	LogOutIcon,
+	PencilIcon,
+} from 'lucide-react'
+import Link from 'next/link'
+import { PropsWithChildren } from 'react'
+import { cn, tw } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import UserAvatar from '@/components/user-avatar'
+
+type Props = Readonly<{
+	login: string
+	displayName: string
+	className?: string
+}>
+
+export default function ProfileSection({
+	login,
+	displayName,
+	className,
+}: Props) {
+	return (
+		<div className={cn('flex flex-row items-center p-2', className)}>
+			<div className="flex flex-row items-center gap-2">
+				<UserAvatar
+					login={login}
+					displayName={displayName}
+					className="h-16 text-2xl"
+				/>
+
+				{login == displayName ? (
+					<span>@{login}</span>
+				) : (
+					<div className="flex flex-col">
+						<span>{displayName}</span>
+						<span>@{login}</span>
+					</div>
+				)}
+			</div>
+			<div className="flex-grow" />
+			<ProfileDropdownMenu>
+				<Button variant="outline" className="p-2">
+					<EllipsisIcon />
+				</Button>
+			</ProfileDropdownMenu>
+		</div>
+	)
+}
+
+function ProfileDropdownMenu({ children }: PropsWithChildren) {
+	const linkClassName = tw`flex h-min w-full justify-start gap-2 p-2 hover:cursor-pointer`
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				<DropdownMenuLabel>Profile</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem asChild>
+					<Link href="/profile" className={linkClassName}>
+						<PencilIcon />
+						<span>Edit Profile</span>
+					</Link>
+				</DropdownMenuItem>
+				<DropdownMenuItem asChild>
+					<Link href="/account/password" className={linkClassName}>
+						<KeyRoundIcon />
+						<span>Change Password</span>
+					</Link>
+				</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem asChild>
+					<Link
+						href="/sign-out"
+						scroll={false}
+						className={linkClassName}
+					>
+						<LogOutIcon />
+						<span>Sign out</span>
+					</Link>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
+}
