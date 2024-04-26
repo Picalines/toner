221ee21c4e04abc657e0b9a540fc99a6efd1cd3c
Picalines <picalines0@gmail.com>
2024-04-26 18:26:42 +0300

    ui: KeyAreaBackground component

---
+++ components/editor/key-area-background.tsx
@@ -0,0 +1,76 @@
+import { ComponentProps, useId } from 'react'
+import { PropsWithoutChildren, cn, tw } from '@/lib/utils'
+
+type Props = PropsWithoutChildren<ComponentProps<'div'>> &
+	Readonly<{
+		lineHeight?: number
+		numberOfLines?: number
+		separatorClassName?: string
+		naturalClassName?: string
+		accidentalClassName?: string
+	}>
+
+export default function KeyAreaBackground({
+	lineHeight = 16,
+	numberOfLines = 12,
+	naturalClassName = tw`fill-white dark:fill-neutral-900`,
+	accidentalClassName = tw`fill-gray-200 dark:fill-neutral-950`,
+	separatorClassName = tw`fill-gray-300 dark:fill-border`,
+	className,
+}: Props) {
+	const patternId = useId() + '-key-bg'
+
+	const accidentalKeys = new Set([1, 3, 5, 8, 10])
+	const keyLines = Array.from({ length: 12 }).map((_, index) => ({
+		index,
+		className: accidentalKeys.has(index)
+			? accidentalClassName
+			: naturalClassName,
+	}))
+
+	const separatorHeight = 0.05
+
+	return (
+		<div
+			className={cn('overflow-hidden', className)}
+			style={{ height: lineHeight * numberOfLines + 'px' }}
+		>
+			<svg width="100%" height="100%" overflow="visible">
+				<pattern
+					id={patternId}
+					viewBox="0 0 1 12"
+					width="100%"
+					y="100%"
+					height={12 * lineHeight}
+					patternUnits="userSpaceOnUse"
+					preserveAspectRatio="xMinYMin meet"
+				>
+					{keyLines.map(({ index, className: lineClassName }) => (
+						<rect
+							key={index}
+							x={0}
+							y={index + separatorHeight / 2}
+							width="100%"
+							height={1 - separatorHeight}
+							className={lineClassName}
+						/>
+					))}
+				</pattern>
+				<rect
+					x={0}
+					y={0}
+					width="100%"
+					height="100%"
+					className={separatorClassName}
+				/>
+				<rect
+					fill={`url(#${patternId})`}
+					x={0}
+					y={0}
+					width="100%"
+					height="100%"
+				/>
+			</svg>
+		</div>
+	)
+}
