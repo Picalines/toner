5156054978093f3d4eeb03f65ef7ec54a32c305f
Picalines <picalines0@gmail.com>
2024-05-03 00:53:58 +0300

    feat: use ScrollArea in main layout

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,7 @@
 import { Loader2 } from 'lucide-react'
 import React, { PropsWithChildren, ReactNode, Suspense } from 'react'
 import ActivitySidebar from '@/components/activity-sidebar'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 import { TooltipProvider } from '@/components/ui/tooltip'
 
 type Props = Readonly<PropsWithChildren & { modal: ReactNode }>
@@ -10,15 +11,18 @@ export default function UserLayout({ children, modal }: Props) {
 		<div className="flex h-[100svh] w-full flex-row">
 			<TooltipProvider>
 				<ActivitySidebar />
-				<main className="relative w-full overflow-auto">
-					<Suspense
-						fallback={
-							<Loader2 className="absolute left-1/2 top-1/2 h-6 w-6 animate-spin" />
-						}
-					>
-						{children}
-					</Suspense>
-				</main>
+				<ScrollArea className="w-full">
+					<main className="relative w-full">
+						<Suspense
+							fallback={
+								<Loader2 className="absolute left-1/2 top-1/2 h-6 w-6 animate-spin" />
+							}
+						>
+							{children}
+						</Suspense>
+					</main>
+					<ScrollBar orientation="vertical" />
+				</ScrollArea>
 				{modal}
 			</TooltipProvider>
 		</div>
