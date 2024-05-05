1084f545df0a20caf9c909a395168caa115edad7
Picalines <picalines0@gmail.com>
2024-05-06 01:10:51 +0300

    feat: Tone context in CompositorEditor

--- components/editor/composition-editor.tsx
+++ components/editor/composition-editor.tsx
@@ -1,25 +1,38 @@
 'use client'
 
+import * as Tone from 'tone'
 import { useEditorStore } from '@/components/providers/editor-store-provider'
 import {
 	ResizableHandle,
 	ResizablePanel,
 	ResizablePanelGroup,
 } from '@/components/ui/resizable'
+import ToneStoreProvider from '../providers/tone-store-provider'
 import KeyEditorPanel from './key-editor-panel'
 
 export default function CompositionEditor() {
 	const panelLayout = useEditorStore(editor => editor.panelLayout)
 
 	return (
-		<ResizablePanelGroup direction={panelLayout} className="flex flex-grow">
-			<ResizablePanel defaultSize={50}>
-				<KeyEditorPanel />
-			</ResizablePanel>
-			<ResizableHandle withHandle />
-			<ResizablePanel defaultSize={50}>
-				{/* TODO: nodes */}
-			</ResizablePanel>
-		</ResizablePanelGroup>
+		<ToneStoreProvider
+			initialState={{
+				context: Tone.getContext(),
+				isAudioAvailable: false,
+				nodes: [],
+			}}
+		>
+			<ResizablePanelGroup
+				direction={panelLayout}
+				className="flex flex-grow"
+			>
+				<ResizablePanel defaultSize={50}>
+					<KeyEditorPanel />
+				</ResizablePanel>
+				<ResizableHandle withHandle />
+				<ResizablePanel defaultSize={50}>
+					{/* TODO: nodes */}
+				</ResizablePanel>
+			</ResizablePanelGroup>
+		</ToneStoreProvider>
 	)
 }
