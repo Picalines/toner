e21e12aab2f580a89cbfd75c538a7b6bfc6633cd
Picalines <picalines0@gmail.com>
2024-05-07 22:35:17 +0300

    ux: merge project & editor dropdowns

--- app/(editor)/projects/[id]/editor-header.tsx
+++ app/(editor)/projects/[id]/editor-header.tsx
@@ -44,10 +44,8 @@ export default function EditorHeader({ className }: Props) {
 				className,
 			)}
 		>
+			<title>{compositionName}</title>
 			<BackButton variant="outline" text={null} />
-			<ProjectDropdownMenu>
-				<Button variant="ghost">{compositionName}</Button>
-			</ProjectDropdownMenu>
 			<EditorDropdownMenu>
 				<Button variant="outline">
 					<EllipsisIcon />
@@ -57,14 +55,18 @@ export default function EditorHeader({ className }: Props) {
 	)
 }
 
-function ProjectDropdownMenu({ children }: PropsWithChildren) {
+function EditorDropdownMenu({ children }: PropsWithChildren) {
+	const compositionName = useCompositionStore(composition => composition.name)
+
 	const compositionId = useCompositionStore(composition => composition.id)
 	const openModal = useEditorStore(editor => editor.openModal)
 
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
-			<DropdownMenuContent align="center">
+			<DropdownMenuContent align="end">
+				<DropdownMenuLabel>{compositionName}</DropdownMenuLabel>
+				<DropdownMenuSeparator />
 				<DropdownMenuItem
 					className="cursor-pointer space-x-2"
 					onClick={() => openModal('updateInfo')}
@@ -82,6 +84,8 @@ function ProjectDropdownMenu({ children }: PropsWithChildren) {
 					</Link>
 				</DropdownMenuItem>
 				<DropdownMenuSeparator />
+				<ViewMenuItems />
+				<DropdownMenuSeparator />
 				<DropdownMenuItem className="cursor-pointer space-x-2 text-destructive">
 					<Trash2Icon />
 					<span>Delete</span>
@@ -91,47 +95,42 @@ function ProjectDropdownMenu({ children }: PropsWithChildren) {
 	)
 }
 
-function EditorDropdownMenu({ children }: PropsWithChildren) {
+function ViewMenuItems() {
 	const panelLayout = useEditorStore(editor => editor.panelLayout)
 	const setPanelLayout = useEditorStore(editor => editor.setPanelLayout) as (
 		newLayout: string,
 	) => void
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuLabel>Editor</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem className="p-0">
-					<ThemeToggle
-						themeName
-						variant="ghost"
-						className="w-full cursor-pointer justify-start space-x-2 p-2"
-					/>
-				</DropdownMenuItem>
-				<DropdownMenuSub>
-					<DropdownMenuSubTrigger className="space-x-2">
-						<LayoutIcon />
-						<span>Layout</span>
-					</DropdownMenuSubTrigger>
-					<DropdownMenuPortal>
-						<DropdownMenuSubContent>
-							<DropdownMenuRadioGroup
-								value={panelLayout}
-								onValueChange={setPanelLayout}
-							>
-								<DropdownMenuRadioItem value="horizontal">
-									Horizontal
-								</DropdownMenuRadioItem>
-								<DropdownMenuRadioItem value="vertical">
-									Vertical
-								</DropdownMenuRadioItem>
-							</DropdownMenuRadioGroup>
-						</DropdownMenuSubContent>
-					</DropdownMenuPortal>
-				</DropdownMenuSub>
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<>
+			<DropdownMenuItem className="p-0">
+				<ThemeToggle
+					themeName
+					variant="ghost"
+					className="w-full cursor-pointer justify-start space-x-2 p-2"
+				/>
+			</DropdownMenuItem>
+			<DropdownMenuSub>
+				<DropdownMenuSubTrigger className="space-x-2">
+					<LayoutIcon />
+					<span>Layout</span>
+				</DropdownMenuSubTrigger>
+				<DropdownMenuPortal>
+					<DropdownMenuSubContent>
+						<DropdownMenuRadioGroup
+							value={panelLayout}
+							onValueChange={setPanelLayout}
+						>
+							<DropdownMenuRadioItem value="horizontal">
+								Horizontal
+							</DropdownMenuRadioItem>
+							<DropdownMenuRadioItem value="vertical">
+								Vertical
+							</DropdownMenuRadioItem>
+						</DropdownMenuRadioGroup>
+					</DropdownMenuSubContent>
+				</DropdownMenuPortal>
+			</DropdownMenuSub>
+		</>
 	)
 }
