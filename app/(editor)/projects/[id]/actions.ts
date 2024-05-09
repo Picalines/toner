593798a0661192f7e7dc8b36003b4ed1a1e9cc07
Picalines <picalines0@gmail.com>
2024-05-09 19:53:52 +0300

    refactor: move fetchComposition to [id]/actions

--- app/(editor)/projects/[id]/actions.ts
+++ app/(editor)/projects/[id]/actions.ts
@@ -2,9 +2,36 @@
 
 import { and, eq } from 'drizzle-orm'
 import { revalidatePath } from 'next/cache'
+import { notFound } from 'next/navigation'
 import { authenticateOrRedirect } from '@/lib/auth'
 import { compositionTable, database } from '@/lib/db'
 import { CompositionInfo } from './schemas'
+
+export async function fetchComposition(
+	accountId: number,
+	compositionId: number,
+) {
+	const compositionQuery = await database
+		.select({
+			id: compositionTable.id,
+			name: compositionTable.name,
+			description: compositionTable.description,
+		})
+		.from(compositionTable)
+		.limit(1)
+		.where(
+			and(
+				eq(compositionTable.authorId, accountId),
+				eq(compositionTable.id, compositionId),
+			),
+		)
+
+	if (compositionQuery.length != 1) {
+		notFound()
+	}
+
+	return compositionQuery[0]
+}
 
 export async function updateCompositionInfo(
 	compositionId: number,
