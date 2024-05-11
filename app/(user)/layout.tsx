3ecb2f91f021932c748e30e2e061fbf7b08620e9
Picalines <picalines0@gmail.com>
2024-05-12 00:18:51 +0300

    fix: fill <main> screen height

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -12,7 +12,7 @@ export default function UserLayout({ children, modal }: Props) {
 			<TooltipProvider>
 				<ActivitySidebar />
 				<ScrollArea className="w-full">
-					<main className="relative w-full">
+					<main className="relative h-[100svh] w-full">
 						<Suspense
 							fallback={
 								<Loader2 className="absolute left-1/2 top-1/2 h-6 w-6 animate-spin" />
