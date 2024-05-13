2e823ae031e13378ccafce1372747aa0d8fef04c
Picalines <picalines0@gmail.com>
2024-05-13 03:22:35 +0300

    components: Slider classname props

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -7,28 +7,42 @@ import { cn } from '@/lib/utils'
 type SliderProps = React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> &
 	Readonly<{
 		thumb?: boolean
+		trackClassName?: string
+		rangeClassName?: string
 	}>
 
 const Slider = React.forwardRef<
 	React.ElementRef<typeof SliderPrimitive.Root>,
 	SliderProps
->(({ className, thumb = true, ...props }, ref) => (
-	<SliderPrimitive.Root
-		ref={ref}
-		className={cn(
-			'relative flex w-full touch-none select-none items-center',
-			className,
-		)}
-		{...props}
-	>
-		<SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-			<SliderPrimitive.Range className="absolute h-full bg-primary" />
-		</SliderPrimitive.Track>
-		{thumb ? (
-			<SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
-		) : null}
-	</SliderPrimitive.Root>
-))
+>(
+	(
+		{ className, trackClassName, rangeClassName, thumb = true, ...props },
+		ref,
+	) => (
+		<SliderPrimitive.Root
+			ref={ref}
+			className={cn(
+				'relative flex h-2 w-full touch-none select-none items-center',
+				className,
+			)}
+			{...props}
+		>
+			<SliderPrimitive.Track
+				className={cn(
+					'relative h-full w-full grow overflow-hidden rounded-full bg-secondary',
+					trackClassName,
+				)}
+			>
+				<SliderPrimitive.Range
+					className={cn('absolute h-full bg-primary', rangeClassName)}
+				/>
+			</SliderPrimitive.Track>
+			{thumb ? (
+				<SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+			) : null}
+		</SliderPrimitive.Root>
+	),
+)
 Slider.displayName = SliderPrimitive.Root.displayName
 
 export { Slider }
